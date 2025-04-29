718d851fcf60213f1cfca929c0bfc21be9f8dd6b
millanee <millane.meyer@uni-ulm.de>
2025-04-29 11:39:48 +0200

    first try of implementation to test if project doable in flutter

--- mc_keyboard/lib/main.dart
+++ mc_keyboard/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,178 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Landscape Keyboard Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LandscapeTypingScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LandscapeTypingScreen extends StatefulWidget {
+  const LandscapeTypingScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LandscapeTypingScreen> createState() => _LandscapeTypingScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LandscapeTypingScreenState extends State<LandscapeTypingScreen> {
+  final String templateText =
+      'This text needs to be typed by the participants.';
+  String typedText = '';
+  bool isShiftActive = false; // shift key state
 
-  void _incrementCounter() {
+  void onKeyPressed(String letter) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (isShiftActive) {
+        typedText += letter.toUpperCase();
+        isShiftActive = false; // turn off shift after one letter was pressed
+      } else {
+        typedText += letter;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body:
+          isLandscape
+              ? Row(
+                children: [
+                  // left side: text (innput)
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            templateText,
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(height: 20),
+                          const Divider(),
+                          const SizedBox(height: 10),
+                          Text(
+                            typedText,
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const VerticalDivider(width: 1),
+                  // Right side: keyboard
+                  Expanded(flex: 2, child: buildKeyboard()),
+                ],
+              )
+              : const Center(
+                child: Text("Please rotate your phone to landscape mode."),
+              ),
+    );
+  }
+
+  Widget buildKeyboard() {
+    const keys = [
+      ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+      ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+      ['z', 'x', 'c', 'v', 'b', 'n', 'm', ';', ':'],
+      ['⇧', '⌫', ' '],
+    ];
+
+    return Transform.rotate(
+      angle: -pi / 2,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            width: constraints.maxHeight,
+            height: constraints.maxWidth,
+            color: Colors.grey[100],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:
+                  keys.map((row) {
+                    return Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: row.map(buildKey).toList(),
+                      ),
+                    );
+                  }).toList(),
             ),
-          ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildKey(String letter) {
+    final isSpace = letter == ' ';
+    final isBackspace = letter == '⌫';
+    final isShift = letter == '⇧';
+
+    // What is displayed:
+    String displayLetter = letter;
+    if (!isShift && !isBackspace && !isSpace && isShiftActive) {
+      displayLetter = letter.toUpperCase();
+    }
+
+    return Expanded(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                if (isShift) {
+                  isShiftActive = !isShiftActive;
+                } else if (isBackspace) {
+                  if (typedText.isNotEmpty) {
+                    typedText = typedText.substring(0, typedText.length - 1);
+                  }
+                } else {
+                  onKeyPressed(isSpace ? ' ' : letter);
+                }
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: EdgeInsets.zero,
+              backgroundColor:
+                  isShift && isShiftActive ? Colors.blue : Colors.white,
+              foregroundColor: Colors.black,
+              elevation: 2,
+            ),
+            child: Transform.rotate(
+              angle: pi / 2,
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  displayLetter,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
